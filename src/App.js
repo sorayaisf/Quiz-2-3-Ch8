3618944c44586709e67292927d780eed4c844310
Soraya Isfandiari <soraya.isf@gmail.com>
2022-05-20 20:43:49 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import List from "./List";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [contacts, setContacts] = useState([
+    {
+      id: 1,
+      name: "John Doe",
+      telp: "08122334455",
+    },
+    {
+      id: 2,
+      name: "Anna Hathaway",
+      telp: "089988676578",
+    },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="px-3 py-3">My Contact List</h1>
+
+      <form className="px-3 py-4">
+        <div className="form-group">
+          <label htmlFor="">Name</label>
+          <input type="text" className="form-control" name="name" />
+        </div>
+        <div className="form-group mt-3">
+          <label htmlFor="">No. Telp</label>
+          <input type="text" className="form-control" name="telp" />
+        </div>
+        <div>
+          <button type="submit" className="btn btn-primary w-100 mt-3">
+            Save
+          </button>
+        </div>
+      </form>
+
+      <List data={contacts} />
     </div>
   );
 }
---
+++ src/HelloAgain.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class HelloAgain extends Component {
+  render() {
+    return (
+      <>
+        <h1>Ini class component</h1>
+      </>
+    );
+  }
+}
+
+export default HelloAgain;
